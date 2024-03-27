c9f84e920326a4d51d81130a2997bec9492f740f
Hugo Silva <78665123+Hasuru@users.noreply.github.com>
2024-03-27 16:25:00 +0000

    (ADDED) Initial Vector3 Utility Library

--- main.cpp
+++ main.cpp
@@ -5,14 +5,15 @@ int main()
     int image_width = 256;
     int image_height = 256;
 
-    std::cout << image_width << ' ' << image_height << "\n255\n";
+    std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
     for (int j = 0; j < image_height; j++)
     {
+        std::clog << "\rScanlines Remaining " << (image_height - j) << ' ' << std::flush;
         for (int i = 0; i < image_width; i++)
         {
             auto r = double(i) / (image_width-1);
-            auto g = double(i) / (image_height-1);
+            auto g = double(j) / (image_height-1);
             auto b = 0;
 
             int ir = static_cast<int>(255.999 * r);
@@ -22,4 +23,6 @@ int main()
             std::cout << ir << ' ' << ig << ' ' << ib << '\n';
         }
     }
+
+    std::clog << "\rDone.                   \n";
 }
