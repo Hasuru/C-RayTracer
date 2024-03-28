81f16ccb840bac9d7c310f53c55ed1ad86e9056c
Hugo Silva <78665123+Hasuru@users.noreply.github.com>
2024-03-28 20:23:36 +0000

    (ADDED) Materials: Lambertian / Metal / dielectric -=- Camera FOV

--- camera.h
+++ camera.h
@@ -4,6 +4,7 @@
 #include "utility.h"
 #include "color.h"
 #include "hittable.h"
+#include "material.h"
 
 #include <iostream>
 
@@ -13,6 +14,8 @@ class camera
         double aspect_ratio = 1.0;
         int image_width = 100;
         int samples_per_pixel = 10;
+        int max_depth = 10;
+        double vfov = 90;
 
         void render(const hittable &world)
         {
@@ -29,7 +32,7 @@ class camera
                     for (int sample = 0; sample < samples_per_pixel; sample++)
                     {
                         ray r = get_ray(i, j);
-                        pixel_color += ray_color(r, world);
+                        pixel_color += ray_color(r, max_depth, world);
                     }
                     write_color(std::cout, pixel_color, samples_per_pixel);
                 }
@@ -53,7 +56,9 @@ class camera
             center = point3(0, 0, 0);
 
             auto focal_length = 1.0;
-            auto viewport_height = 2.0;
+            auto theta = degrees_to_radians(vfov);
+            auto h = tan(theta/2);
+            auto viewport_height = 2.0 * h * focal_length;
             auto veiwport_width = viewport_height * (static_cast<double>(image_width) / image_height);
 
             auto viewport_u = vec3(veiwport_width, 0, 0);
@@ -84,16 +89,23 @@ class camera
             return (px * pixel_delta_u) + (py * pixel_delta_v);
         }
 
-        color ray_color(const ray &ray, const hittable& world) const
+        color ray_color(const ray &r, int depth, const hittable& world) const
         {
             hit_record rec;
 
-            if (world.hit(ray, interval(0, infinity), rec))
+            if (depth <= 0) return color(0, 0, 0);
+
+            if (world.hit(r, interval(0.001, infinity), rec))
             {
-                return 0.5 * (rec.normal + color(1, 1, 1));
+                ray scattered;
+                color attenuation;
+
+                if (rec.mat->scatter(r, rec, attenuation, scattered)) return attenuation * ray_color(scattered, depth-1, world);
+
+                return color(0, 0, 0);
             }
             
-            vec3 unit_direction = unit_vector(ray.direction());
+            vec3 unit_direction = unit_vector(r.direction());
             auto a = 0.5 * (unit_direction.y() + 1.0);
             return (1.0 - a) * color(1.0, 1.0, 1.0) + a * color(0.5, 0.7, 1.0);
         }
